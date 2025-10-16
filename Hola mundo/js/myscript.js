35208326e4daa290f882adbd3498e835fbf863ec
JoseCarlosMoraC <josecarlosmorac@gmail.com>
2025-10-16 14:17:42 +0200

    Nuevo

--- Hola mundo/js/myscript.js
+++ Hola mundo/js/myscript.js
@@ -138,20 +138,36 @@ console.log(second_div);
 
 console.log($("#my_third_div"));
 console.log($(".pruebas"))
-$("btn").addEventListener("click",function(){
-  var input = document.createElement("input");
-  input.setAttribute("type","email");
-  input.setAttribute("placeholder","E-mail");
-  input.setAttribute("name","emails[]");
-  $("#form").appendChild(input);
-  myAlert("Add new email input");
 
+$("#btn").addEventListener("click",function(){
+    var input = document.createElement("input");
+    input.setAttribute("type","email")
+    input.setAttribute("placeholder","E-mail")
+    input.setAttribute("name","emails[]")
+    $("#form").appendChild(input)
+    myAlert("Add a new email input")
 });
 
 function myAlert(msg){
-  var div= document.createElement ("div");
-  div.classList.add("alert");
-  div.innerHTML = msg;
-  $("body").insertBefore(div,$("body").firstChild);
-}
+    var div = document.createElement("div");
+    div.classList.add("alert")
+    div.innerHTML = msg
+    var close = document.createElement("span");
+    close.style.float="right";
+    close.classList.add("close");
+    close.innerHTML = "x";
+    div.appendChild(close)
+    $("body").insertBefore(div,$("body").firstChild)
+    bind_close();
+} 
 
+function bind_close(){
+    let elements = document.querySelectorAll(".close");
+    for(var i=elements.length-1;i>=0;i--){
+        let el = elements[i];
+        el.addEventListener("click",function(){
+            this.parentNode.style.display="none";
+
+        });
+    }
+}
